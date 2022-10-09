d9bc372afd705b377a3e385d86e9617f91c99109
Edwin3002 <vargas11742@gmail.com>
2022-10-08 19:01:28 -0500

    pipe 1

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Navb from "../components/nav/NavB";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+     <Head>
+          <title>Reivid Sports</title>
+          <link rel = "icon" href="https://res.cloudinary.com/edwin3002/image/upload/v1665242470/felipe/logo-rs2_pjpetw.png" type = "image/x-icon"/>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+      <Navb />
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
